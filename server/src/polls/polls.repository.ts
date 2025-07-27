6db425650def81a1ce7e51838b3ed98d53c885c4
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-07-27 09:58:15 +0300

    feature/add polls repository

---
+++ server/src/polls/polls.repository.ts
@@ -0,0 +1,123 @@
+import { Inject, Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from "ioredis";
+import { IORedisKey } from "src/redis/redis.module";
+import { CreatePollData } from "src/types/repository-types/create-poll.type";
+import { Poll } from 'shared'
+import { AddParticipantData } from "src/types/repository-types/add-participant.type";
+
+@Injectable()
+export class PollsRepository {
+    private readonly ttl: string; // time-to-live
+    private readonly logger = new Logger(PollsRepository.name);
+
+    constructor(
+        configService: ConfigService,
+        @Inject(IORedisKey) private readonly redisClient: Redis,
+    ) {
+        this.ttl = configService.get('POLL_DURATION') || '7200';
+    }
+
+    async createPoll({
+        votesPerVoter,
+        topic,
+        pollID,
+        userID
+    }: CreatePollData): Promise<Poll> {
+        const initialPoll = {
+            id: pollID,
+            topic,
+            votesPerVoter,
+            participants: {},
+            adminID: userID
+        };
+        this.logger.log(
+            `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL
+            ${this.ttl}`
+        );
+
+        const key = `polls:${pollID}`;
+        try {
+            await this.redisClient.multi([
+                ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
+                ['expire', key, this.ttl],
+            ]).exec();
+            return initialPoll;
+        } catch (error) {
+            this.logger.error(
+                `Failed to add poll ${JSON.stringify(initialPoll)}\n${error}`
+            );
+            throw new InternalServerErrorException();
+        }
+    }
+    
+    async getPoll(pollID: string): Promise<Poll> {
+        this.logger.log(`Attempting to get poll with: ${pollID}`);
+        const key = `polls:${pollID}`;
+
+        try {
+            const currentPoll = await this.redisClient.call('JSON.GET', key, '.') as string;
+
+            this.logger.verbose(currentPoll);
+
+            if (!currentPoll || typeof currentPoll !== 'string') {
+                throw new InternalServerErrorException('Poll data is missing or corrupted');
+            }
+
+            return JSON.parse(currentPoll);
+        } catch (e) {
+            this.logger.error(`Failed to get pollID ${pollID}: ${e.message}`);
+            throw e;
+        }
+    }
+
+
+    async addParticipant({
+        pollID,
+        userID,
+        name,
+    }: AddParticipantData): Promise<Poll> {
+        this.logger.log(
+        `Attempting to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+        );
+
+        const key = `polls:${pollID}`;
+        const participantPath = `.participants.${userID}`;
+
+        try {
+            await this.redisClient.call(
+                'JSON.SET',
+                key,
+                participantPath,
+                JSON.stringify(name),
+            );
+
+            const pollJSON = await this.redisClient.call(
+                'JSON.GET',
+                key,
+                '.',
+            ) as string;
+            
+            if (!pollJSON || typeof pollJSON !== 'string') {
+                throw new InternalServerErrorException('Poll data is missing or corrupted');
+            }
+
+            const poll = JSON.parse(pollJSON) as Poll;
+
+            this.logger.debug(
+                `Current Participants for pollID: ${pollID}:`,
+                poll.participants,
+            );
+
+            return poll;
+        } catch (e) {
+            this.logger.error(
+                `Failed to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+            );
+            throw e;
+        }
+    }
+
+
+
+}
