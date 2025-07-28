ec6f3da100c4a087bf3e3e471951fb0c8154ef84
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-07-28 07:30:32 +0300

    fix/add RequestWithAuth type after use Request from express

--- server/src/guards/auth.guard.ts
+++ server/src/guards/auth.guard.ts
@@ -1,15 +1,16 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable, Logger } from "@nestjs/common";
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from "rxjs";
+import { RequestWithAuth } from "src/types/auth-payload.type";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     private readonly logger = new Logger(AuthGuard.name);
     constructor(private readonly jwtService: JwtService) {}
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const request = context.switchToHttp().getRequest();
+        const request: RequestWithAuth = context.switchToHttp().getRequest();
 
-        this.logger.debug(`Checking for auth token on request body`, request.Body);
+        this.logger.debug(`Checking for auth token on request body`, request.body);
         const {accessToken} = request.body;
         try {
             const payload = this.jwtService.verify(accessToken);
--- server/src/types/auth-payload.type.ts
+++ server/src/types/auth-payload.type.ts
@@ -1,4 +1,4 @@
-import { Request } from '@nestjs/common' 
+import { Request } from 'express' 
 
 type AuthPayload = {
     userID: string;
